09d8ca543e08792ea20f87b3420086f7daeb0b73
Robert Alexander <rla@navadrag.com>
2018-12-24 16:38:55 -0500

    taking-a-break

--- OSRS Duel Arena/osrs.sol
+++ OSRS Duel Arena/osrs.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.4.20;
+// pragma solidity ^0.5.0;
 
 /*
 TODO
@@ -34,6 +35,7 @@ contract osrs is roll {
 
   uint private decFactor = 1000000000000;
 
+
   function duel(player _player1, player _player2) internal returns (player){
 
       uint p1Acc = accuracy(_player1.atkRoll(), _player2.defRoll());
@@ -43,52 +45,43 @@ contract osrs is roll {
       uint p2Max = _player2.maxHit();
 
       uint round = 0;
-      while (health(_player1) > 0 && health(_player2) > 0) {
+      while (health(_player1) != 0 && health(_player2) != 0 && round <= 15) {
+          require(round < 5);
           uint p1Roll = rand(0, SafeMath.add(decFactor, 1));
           uint p2Roll = rand(0, SafeMath.add(decFactor, 1));
-          uint p1Pois = rand(0, 4);
-          uint p2Pois = rand(0, 4);
-          if (p1Roll < p1Acc){
-              if (p1Pois == 3){
-                  _player2.poisoned(round);
-              }
-              uint hitsplatP2 = rand(0, p1Max);
-              _player2.deductHP(hitsplatP2, "hit");
-          }
+
+          if (p1Acc > p1Roll){  hit(_player2, p1Max, round); }
           else{ _player2.deductHP(0, "miss"); }
-          if (p2Roll < p2Acc){
-              if (p2Pois == 3){
-                  _player1.poisoned(round);
-              }
-              uint hitsplatP1 = rand(0, p2Max);
-              _player1.deductHP(hitsplatP1, "hit");
-          }
-          else{ _player1.deductHP(0, "miss"); }
+          if (p2Acc > p2Roll){  hit(_player1, p2Max, round); }
+          else{ _player1.deductHP(0, "miss"); }//i bet they they never miss huh?
 
-          if (health(_player1) > 0) {	applyPoison(_player1, round); }
-          if (health(_player2) > 0) {	applyPoison(_player2, round); }
+          if (health(_player1) > 0) {  applyPoison(_player1, round); }
+          if (health(_player2) > 0) {  applyPoison(_player2, round); }
 
-          round = SafeMath.add(round, 1);
+          round += 1;
       }
-      if (health(_player1) <= 0 && health(_player2) <= 0){
-          return (_player1.getPID() < _player2.getPID() ? _player1 : _player2);
-      }
-      return (health(_player2) > 0 ? _player2 : _player1);
+
+
+      if (health(_player1) == 0 && health(_player2) == 0) {return (_player1.getPID() < _player2.getPID() ? _player1 : _player2); }
+      if (health(_player1) > 0 && !(health(_player2) > 0)) {  return _player1; }
+      else if (health(_player2) > 0 && !(health(_player1) > 0)) { return _player2; }
+      else{ return (health(_player1) > health(_player2) ? _player1 : _player2); }
+
+
   }
 
 
   function accuracy(uint atk, uint def) internal view returns (uint){
       if (atk > def) {
-          return SafeMath.div(SafeMath.mul(SafeMath.add(def,2), decFactor), SafeMath.mul(SafeMath.add(atk, 1), 2));
+          return SafeMath.sub(decFactor, SafeMath.div(SafeMath.mul(SafeMath.add(def,2), decFactor), SafeMath.mul(SafeMath.add(atk, 1), 2)));
       }
       else{
           return SafeMath.div(SafeMath.mul(atk, decFactor), SafeMath.mul(2, SafeMath.add(def, 1)));
-
       }
   }
 
 
-  function applyPoison(player _player, uint round) public{
+  function applyPoison(player _player, uint round) internal{
       if (_player.checkPoisoned()){
           uint poisonTicked = SafeMath.sub(round, _player.whenPoisoned());
           if (poisonTicked >= 7){
@@ -96,6 +89,15 @@ contract osrs is roll {
               _player.poisoned(round);
           }
       }
+  }
+
+  function hit(player _player, uint _otherMaxhit, uint _round) internal{
+      uint pois = rand(0, 4);
+      if (pois == 3){
+          _player.poisoned(_round);
+      }
+      uint hitsplat = rand(0, _otherMaxhit);
+      _player.deductHP(hitsplat, "hit");
   }
 
 
@@ -117,7 +119,8 @@ contract DuelArena is osrs{
   }
   uint tax;
   address public owner;
-  address payable[] addresses;
+  // address payable[] addresses; 0.5.0
+  address[] addresses;
   uint public minBet;
   uint public totalBet;
   string public recentWinner;
@@ -149,11 +152,11 @@ contract DuelArena is osrs{
       uint winnings = SafeMath.sub(totalBet, taxed);
 
       for(uint i = 0; i < addresses.length; i++){
-          address payable playerAddress = addresses[i];
+          address playerAddress = addresses[i];
           bets[playerAddress].user.reset();
           if(bets[playerAddress].user == _winner){
               playerAddress.transfer(winnings);
-              /* recentWinner = bets[playerAddress].user.getName(); */
+              recentWinner = bets[playerAddress].user.getName();
               /* recentwinninghit = bets[playerAddress].user.getHitsplats()[0]; */
           }
       }
